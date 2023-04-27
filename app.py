90b6d8e86d883d667da614dbeb7f5678feef2892
Menashe <mborukhov770@gmail.com>
2023-04-27 16:07:51 -0600

    app.py done

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ def questions():
     """Generate and show form to ask words."""
 
     prompts = story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """show story"""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
